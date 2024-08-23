69a6f05e41f4bbe56c94b1a477be62af79e30105
devNothingfirst <dev.nothingfirst@gmail.com>
2024-08-23 21:31:44 +0530

    first commit

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
 
-import { Navbar, Footer, Landing, About, Skills, Testimonials, Education, Experience, Contacts, Projects} from '../../components'
+import { Navbar, Footer, Landing, About, Skills, Education, Experience, Contacts, Projects} from '../../components'
 import { headerData } from '../../data/headerData'
 
 function Main() {
@@ -19,7 +19,7 @@ function Main() {
             <Projects />
             {/* <Achievement /> */}
             {/* <Services /> */}
-            <Testimonials />
+            {/* <Testimonials /> */}
             {/* <Blog /> */}
             <Contacts />
             <Footer />
